f368bc3c52f2269624d6a6b9a74a257c23ed69eb
Eric Addai <ericaddai828@outlook.com>
2025-08-11 17:22:57 +0000

    Fix case sensitivity for Toaster import

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@
 import { BrowserRouter, Route, Routes, } from "react-router-dom"
 import { Home } from "./Pages/Home"
 import { NotFound } from "./Pages/NotFound"
-import { Toaster } from "@/components/ui/toaster";
-
-
+import { Toaster } from "@/components/ui/toaster.jsx";
 
 function App() {
 
