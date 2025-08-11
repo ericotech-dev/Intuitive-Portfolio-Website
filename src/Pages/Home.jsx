3d9ca1834c5afd097d11dc82349fee1783284315
Eric Addai <ericaddai828@outlook.com>
2025-08-11 18:01:14 +0000

    Modified Home file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import { StarBackground } from "@/components/StarBackground";
-import { ThemeToggle } from "../Components/ThemeToggle";
+import { ThemeToggle } from "../components/ThemeToggle";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
